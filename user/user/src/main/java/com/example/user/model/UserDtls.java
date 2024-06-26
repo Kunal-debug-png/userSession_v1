29844bd6c101dd71f0af437d501abb9d0495e177
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-06-26 23:15:54 +0530

    user db

--- user/user/src/main/java/com/example/user/model/UserDtls.java
+++ user/user/src/main/java/com/example/user/model/UserDtls.java
@@ -1,2 +1,21 @@
-package com.example.user.model;public class UserDtls {
+package com.example.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class UserDtls {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String fullName;
+    private String email;
+    private String address;
+    private String qualification;
+    private String password;
+    private String role;
 }

e7b88c83f9d481b9968e4b5a81a0cc63ab7ef918
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-06-28 23:20:28 +0530

    Security config

--- user/user/src/main/java/com/example/user/configuration/SecurityConfig.java
+++ user/user/src/main/java/com/example/user/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.user.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
